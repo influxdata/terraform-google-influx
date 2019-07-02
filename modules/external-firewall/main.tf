d80790238beb9fdae41d6c83d05704e2333639b1
Petri Autero <petri@gruntwork.io>
2019-07-03 00:27:44 +0300

    Formatting

--- modules/external-firewall/main.tf
+++ modules/external-firewall/main.tf
@@ -1,11 +1,11 @@
-# ---------------------------------------------------------------------------------------------------------------------
-# CREATE FIREWALL RULES FOR EXTERNAL TRAFFIC
-# ---------------------------------------------------------------------------------------------------------------------
-
 terraform {
   # This module has been updated with 0.12 syntax, which means it is no longer compatible with any versions below 0.12.
   required_version = ">= 0.12"
 }
+
+# ---------------------------------------------------------------------------------------------------------------------
+# CREATE FIREWALL RULES FOR EXTERNAL TRAFFIC
+# ---------------------------------------------------------------------------------------------------------------------
 
 resource "google_compute_firewall" "external" {
   project = var.project
