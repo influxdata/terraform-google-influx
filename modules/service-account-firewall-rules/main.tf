6d7f262dfd70d72995af660efd6e0e97aff6add4
Petri Autero <petri@gruntwork.io>
2019-07-03 00:28:14 +0300

    Formatting

--- modules/service-account-firewall-rules/main.tf
+++ modules/service-account-firewall-rules/main.tf
@@ -1,11 +1,11 @@
-# ---------------------------------------------------------------------------------------------------------------------
-# CREATE FIREWALL RULES FOR SERVICE ACCOUNT
-# ---------------------------------------------------------------------------------------------------------------------
-
 terraform {
   # This module has been updated with 0.12 syntax, which means it is no longer compatible with any versions below 0.12.
   required_version = ">= 0.12"
 }
+
+# ---------------------------------------------------------------------------------------------------------------------
+# CREATE FIREWALL RULES FOR SERVICE ACCOUNT
+# ---------------------------------------------------------------------------------------------------------------------
 
 resource "google_compute_firewall" "internal" {
   project = var.project
