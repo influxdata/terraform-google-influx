a0ed122147eec25ffee106822835dbe1c149821e
Petri Autero <petri@gruntwork.io>
2019-07-03 00:23:32 +0300

    Upgrade internal-firewall

--- modules/external-firewall/variables.tf
+++ modules/external-firewall/variables.tf
@@ -5,19 +5,22 @@
 
 variable "project" {
   description = "ID of the GCP Project where all resources will be launched."
+  type        = string
 }
 
 variable "name_prefix" {
   description = "This variable is used to namespace all resources created by this module."
+  type        = string
 }
 
 variable "target_tags" {
   description = "The target tags define the compute instances to which the rules apply."
-  type        = "list"
+  type        = list(string)
 }
 
 variable "network" {
   description = "The name or self link of the VPC network in which to deploy the InfluxDB cluster"
+  type        = string
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -27,17 +30,19 @@ variable "network" {
 
 variable "ports" {
   description = "An optional list of ports or port ranges to which this rule applies. If not specified, this rule applies to connections through any port."
-  type        = "list"
+  type        = list(string)
   default     = []
 }
 
 variable "protocol" {
   description = "The IP protocol to which this rule applies."
+  type        = string
   default     = "tcp"
 }
 
 variable "allow_access_from_cidr_blocks" {
   description = "The list of CIDR-formatted IP address ranges from which access will be allowed."
-  type        = "list"
+  type        = list(string)
   default     = []
 }
+
