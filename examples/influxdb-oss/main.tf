6be26cc56c1343ee3c5136c85d99993aa9b246e5
Petri Autero <petri@gruntwork.io>
2019-07-03 16:53:09 +0300

    Upgrade influxdb oss example

--- examples/influxdb-oss/main.tf
+++ examples/influxdb-oss/main.tf
@@ -9,14 +9,20 @@
 
 provider "google" {
   version = "~> 2.7.0"
-  region  = "${var.region}"
-  project = "${var.project}"
+  region  = var.region
+  project = var.project
 }
 
 provider "google-beta" {
   version = "~> 2.7.0"
-  region  = "${var.region}"
-  project = "${var.project}"
+  region  = var.region
+  project = var.project
+}
+
+terraform {
+  # The modules used in this example have been updated with 0.12 syntax, which means the example is no longer
+  # compatible with any versions below 0.12.
+  required_version = ">= 0.12"
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -30,29 +36,33 @@ module "influxdb_oss" {
   # source = "github.com/gruntwork-io/terraform-google-influx.git//modules/tick-instance-group?ref=v0.0.1"
   source = "../../modules/tick-instance-group"
 
-  project = "${var.project}"
-  region  = "${var.region}"
+  project = var.project
+  region  = var.region
 
   // Size of an OSS setup is always 1
   size = 1
 
-  data_volume_size        = 50
-  data_volume_device_name = "influxdb"
-  network_tag             = "${var.name}"
-  name                    = "${var.name}"
-  machine_type            = "${var.machine_type}"
-  image                   = "${var.image}"
-  startup_script          = "${data.template_file.startup_script.rendered}"
-  network                 = "default"
+  persistent_volumes = [
+    {
+      device_name = "influxdb"
+      size        = 10
+      // For the example, we want to delete the data volume on 'terraform destroy'
+      auto_delete = true
+    }
+  ]
 
-  // For the example, we want to delete the data volume on 'terraform destroy'
-  data_volume_auto_delete = "true"
+  network_tag    = var.name
+  name           = var.name
+  machine_type   = var.machine_type
+  image          = var.image
+  startup_script = data.template_file.startup_script.rendered
+  network        = "default"
 
   // To make testing easier, we're assigning public IPs to the node and allowing traffic from all IP addresses
-  assign_public_ip = "true"
+  assign_public_ip = true
 
   // Use the custom InfluxDB SA
-  service_account_email = "${module.service_account.email}"
+  service_account_email = module.service_account.email
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -65,7 +75,7 @@ module "service_account" {
   # source = "github.com/gruntwork-io/terraform-google-influx.git//modules/tick-service-account?ref=v0.0.1"
   source = "../../modules/tick-service-account"
 
-  project      = "${var.project}"
+  project      = var.project
   name         = "${var.name}-sa"
   display_name = "Service Account for InfluxDB OSS Server ${var.name}"
 }
@@ -81,11 +91,12 @@ module "external_firewall" {
   # source = "github.com/gruntwork-io/terraform-google-influx.git//modules/external-firewall?ref=v0.0.1"
   source = "../../modules/external-firewall"
 
-  name_prefix = "${var.name}"
+  name_prefix = var.name
   network     = "default"
-  project     = "${var.project}"
-  target_tags = ["${var.name}"]
+  project     = var.project
+  target_tags = [var.name]
 
+  // To make testing easier, we're allowing traffic from all IP addresses
   allow_access_from_cidr_blocks = ["0.0.0.0/0"]
 }
 
@@ -94,11 +105,12 @@ module "external_firewall" {
 # ---------------------------------------------------------------------------------------------------------------------
 
 data "template_file" "startup_script" {
-  template = "${file("${path.module}/startup-script.sh")}"
+  template = file("${path.module}/startup-script.sh")
 
-  vars {
+  vars = {
     disk_device_name = "influxdb"
     disk_mount_point = "/influxdb"
     disk_owner       = "influxdb"
   }
 }
+
--- examples/influxdb-oss/outputs.tf
+++ examples/influxdb-oss/outputs.tf
@@ -1,9 +1,10 @@
 output "influxdb_instance_group" {
   description = "Self link to the InfluxDB instance group"
-  value       = "${module.influxdb_oss.instance_group}"
+  value       = module.influxdb_oss.instance_group
 }
 
 output "influxdb_instance_group_manager" {
   description = "Self link of the InfluxDB instance group"
-  value       = "${module.influxdb_oss.instance_group_manager}"
+  value       = module.influxdb_oss.instance_group_manager
 }
+
